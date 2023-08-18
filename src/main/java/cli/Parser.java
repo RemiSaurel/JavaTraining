15deab1119d78c01bd386251b83bcb377212ab8a
RemiSaurel <r.saurel5@gmail.com>
2023-08-19 00:01:05 +0200

    add: parser and printer

--- src/main/java/cli/Parser.java
+++ src/main/java/cli/Parser.java
@@ -1,2 +1,35 @@
-package cli;public class Parser {
+package cli;
+
+public class Parser {
+
+    public void parse(String line) {
+        String[] words = line.split(" ");
+        if (words.length == 0) {
+            return;
+        }
+        String command = words[0];
+        String[] arguments = new String[words.length - 1];
+        for (int i = 1; i < words.length; i++) {
+            arguments[i - 1] = words[i];
+        }
+        switch (command) {
+            case "-h":
+                Printer.help(Main.COMMANDS);
+                break;
+            case "-q":
+                Printer.goodbye();
+                System.exit(0);
+                break;
+            case "-c":
+                if (arguments.length == 0) {
+                    System.out.println("Le nom du projet est obligatoire");
+                } else {
+                    System.out.println("Création du projet " + arguments[0]);
+                }
+                break;
+            default:
+                System.out.println("Commande inconnue");
+                break;
+        }
+    }
 }
--- src/main/java/cli/Printer.java
+++ src/main/java/cli/Printer.java
@@ -1,2 +1,36 @@
-package cli;public class Printer {
+package cli;
+
+public class Printer {
+
+    public static void welcome() {
+        System.out.println("Bienvenue dans le projet de CLI !");
+    }
+
+    public static void commandList() {
+        System.out.println("Liste des commandes :");
+    }
+
+    public static void newline() {
+        System.out.print("> ");
+    }
+
+    public static void help(Command[] commands) {
+        Printer.commandList();
+        for (Command command : commands) {
+            System.out.print(
+                    "-" + command.getLetter() +
+                    "\t" +
+                    "--" + command.getWord());
+            if (command.getArguments() != null) {
+                for (String argument : command.getArguments()) {
+                    System.out.print(" <" + argument + "> ");
+                }
+            }
+            System.out.println("\t" + command.getHelp());
+        }
+    }
+
+    public static void goodbye() {
+        System.out.println("Au revoir !");
+    }
 }
