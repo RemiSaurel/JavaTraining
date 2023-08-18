78363d39e3d3b735806273ab6c2a7c44ccbe46be
RemiSaurel <r.saurel5@gmail.com>
2023-08-19 00:00:58 +0200

    add: commandfactory

--- src/main/java/cli/CommandFactory.java
+++ src/main/java/cli/CommandFactory.java
@@ -1,2 +1,12 @@
-package cli;public class CommandFactory {
+package cli;
+
+public class CommandFactory {
+
+    public static Command create(char letter, String word, String[] arguments, String help) {
+        return new Command(letter, word, arguments, help);
+    }
+
+    public static Command create(char letter, String word, String help) {
+        return new Command(letter, word, null, help);
+    }
 }
