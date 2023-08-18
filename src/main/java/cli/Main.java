31449fb9c43e29839554053f81d7da2d7ceee6ee
RemiSaurel <r.saurel5@gmail.com>
2023-08-19 00:01:17 +0200

    add: main for cli

--- src/main/java/cli/Main.java
+++ src/main/java/cli/Main.java
@@ -1,2 +1,23 @@
-package cli;public class Main {
+package cli;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static final Command[] COMMANDS = {
+        CommandFactory.create('h', "help", "Afficher l'aide"),
+        CommandFactory.create('q', "quit", "Quitter l'application"),
+        CommandFactory.create('c', "create", new String[] {"name"}, "Créer un nouveau projet"),
+    };
+    public static void main(String[] args) {
+        Printer.welcome();
+        Printer.help(COMMANDS);
+        Scanner scanner = new Scanner(System.in);
+        Parser parser = new Parser();
+        while (true) {
+            Printer.newline();
+            String line = scanner.nextLine();
+            parser.parse(line);
+        }
+    }
 }
